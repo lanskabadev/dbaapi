3c2ff02e520ee9e1e7cc0078132b1e6be8881e1f
Lans Kaba <115970488+lanskabadev@users.noreply.github.com>
2022-10-29 00:18:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ const morgan = require("morgan");
 var cors = require("cors");
 const path = require("path");
 
-// const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const categoryRoute = require("./routes/category");
 const postRoute = require("./routes/posts");
 const settingsRoute = require("./routes/settings");
-
-// const uploadRoute = require("./routes/upload");
-// const useruploadRoute = require("./routes/userupload");
-// const messageRoute = require("./routes/messages");
-
+const uploadRoute = require("./routes/upload");
+const resumeRoute = require("./routes/resume");
 dotenv.config();
 
 mongoose.connect(
@@ -26,21 +22,22 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
-app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+const publicDirectoryPath = path.join(__dirname, "public/uploads");
+app.use("/public/uploads", express.static(publicDirectoryPath));
+
 // Middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 app.use(cors());
 
-// app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/category", categoryRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/settings", settingsRoute);
-// app.use("/api/upload", uploadRoute);
-// app.use("/api/userupload", useruploadRoute);
-// app.use("/api/messages", messageRoute);
+app.use("/api/upload", uploadRoute);
+app.use("/api/resume", resumeRoute);
 
 app.listen(8000, () => {
   console.log("server running");
